c7fbc47ca5b30f9512abaf1db639c0490c44c86b
Pritesh2k <priteshparekh237@gmail.com>
2023-09-14 17:45:52 +0100

    Added the unit test for getRatio and getdataPoint

--- client_test.py
+++ client_test.py
@@ -1,13 +1,21 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
+
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+
+
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(stock, 'ABC')
+    self.assertEqual(bid_price, 120.48)
+    self.assertEqual(ask_price, 121.2)
+    self.assertEqual(price, (120.48 + 121.2) / 2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -16,9 +24,30 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    """ Test if the function correctly calculates the price when bid is greater than ask """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(stock, 'ABC')
+    self.assertEqual(bid_price, 120.48)
+    self.assertEqual(ask_price, 119.2)
+    self.assertEqual(price, (120.48 + 119.2) / 2)
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio(self):
+    """ Test if the function correctly calculates the ratio """
+    ratio = getRatio(20, 10)
+    self.assertEqual(ratio, 2)
+
+    # Test division by zero
+    ratio = getRatio(20, 0)
+    self.assertIsNone(ratio)
+
+    # Additional test cases
+    ratio = getRatio(0, 20)
+    self.assertEqual(ratio, 0)
+
+    ratio = getRatio(0, 0)
+    self.assertIsNone(ratio)
 
 
 if __name__ == '__main__':
